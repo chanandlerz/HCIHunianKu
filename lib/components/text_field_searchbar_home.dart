d0306058b555adf1375f7c47b6d7f39686e93751
chanandlerz <lubisputri16@gmaiil.com>
2023-12-11 10:20:09 +0700

    HCI

---
+++ lib/components/text_field_searchbar_home.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class SearchTextFieldHomePage extends StatelessWidget {
+  final controller;
+  final String hintText;
+  final bool obscureText;
+
+  const SearchTextFieldHomePage({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        obscureText: obscureText,
+        controller: controller,
+        decoration: InputDecoration(
+            hintStyle: const TextStyle(fontSize: 15),
+            enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Color.fromARGB(255, 205, 166, 122)),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey.shade400),
+            ),
+            fillColor: Colors.grey.shade200,
+            filled: true,
+            contentPadding:
+                const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+            hintText: hintText,
+            prefixIcon: const Icon(
+              Icons.location_on,
+              color: Color.fromARGB(255, 205, 166, 122),
+            )
+            // suffixIcon: Icon(
+            //   Icons.notifications_none,
+            //   color: Color.fromARGB(255, 205, 166, 122),
+            // )
+            ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/property_page.dart
@@ -0,0 +1,199 @@
+import 'package:app_development/components/home_page_grid.dart';
+import 'package:app_development/components/filter_property_beli.dart';
+import 'package:app_development/components/text_field_searchbar_home.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+
+  final searchbarController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final String query = ModalRoute.of(context)!.settings.arguments as String;
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text("Home Page"),
+      // ),
+      backgroundColor: const Color.fromARGB(255, 58, 58, 58),
+      body: ListView(
+        children: <Widget>[
+          // Row(
+          //   mainAxisAlignment: MainAxisAlignment.start,
+          //   mainAxisSize: MainAxisSize.max,
+          //   crossAxisAlignment: CrossAxisAlignment.center,
+          //   children: [
+          //     Icon(
+          //       Icons.favorite,
+          //       color: Colors.pink,
+          //       size: 20.0,
+          //     ),
+
+          //     SearchTextField(
+          //       controller: searchbarController,
+          //       hintText: 'Find Property, Land, and more...',
+          //       obscureText: false,
+          //     )
+          //   ],
+          // )
+
+          Column(
+            children: [
+              Text('test: $query'),
+              const SizedBox(
+                height: 20,
+              ),
+
+              // Container(
+              //   padding: EdgeInsets.all(10.0),
+              //   child: Row(
+              //     mainAxisAlignment: MainAxisAlignment.start,
+              //     mainAxisSize: MainAxisSize.max,
+              //     crossAxisAlignment: CrossAxisAlignment.center,
+              //     children: [
+              //       Icon(
+              //         Icons.favorite,
+              //         color: Colors.pink,
+              //         size: 20.0,
+              //       ),
+              //       SearchTextField(
+              //         controller: searchbarController,
+              //         hintText: 'Find Property, Land, and more...',
+              //         obscureText: false,
+              //       )
+              //     ],
+              //   ),
+              // )
+
+              // Icon(
+              //   Icons.favorite,
+              //   color: Colors.pink,
+              //   size: 20.0,
+              // ),
+
+              // Row(
+              //   children: [
+              //     SearchTextField(
+              //       controller: searchbarController,
+              //       hintText: 'Find Property, Land, and more...',
+              //       obscureText: false,
+              //     ),
+              //     Container(
+              //       width: 200,
+              //       height: 200,
+              //       decoration: BoxDecoration(
+              //         color: Colors.red,
+              //       ),
+              //     ),
+              //   ],
+              // )
+
+              SearchTextFieldHomePage(
+                controller: searchbarController,
+                hintText: 'Jakarta Timur',
+                obscureText: false,
+              ),
+
+              const SizedBox(
+                height: 10,
+              ),
+
+              const PropertyWidget(),
+            ],
+          ),
+        ],
+      ),
+
+      // bottomNavigationBar: CurvedNavigationBar(
+      //   backgroundColor: Colors.transparent,
+      //   onTap: (index) {},
+      //   height: 50,
+      //   color: Color.fromARGB(255, 58, 58, 58),
+      //   items: [
+      //     IconButton(
+      //         onPressed: () {
+      //           Navigator.push(
+      //               context, MaterialPageRoute(builder: (_) => HomePage()));
+      //         },
+      //         icon: Icon(
+      //           Icons.home_filled,
+      //           size: 30,
+      //           color: Color.fromARGB(255, 205, 166, 122),
+      //         )),
+      //     // Icon(
+      //     //   Icons.home_filled,
+      //     //   size: 30,
+      //     //   color: Color.fromARGB(255, 205, 166, 122),
+      //     // ),
+      //     Icon(
+      //       Icons.favorite_border,
+      //       size: 30,
+      //       color: Color.fromARGB(255, 205, 166, 122),
+      //     ),
+      //     Icon(
+      //       Icons.person,
+      //       size: 30,
+      //       color: Color.fromARGB(255, 205, 166, 122),
+      //     ),
+      //   ],
+      // ),
+      //////////////////////////////
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      //       return AboutPage();
+      //     }));
+      //   },
+      //   child: Icon(Icons.keyboard_arrow_right),
+      // ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+// class HomePageGrid extends StatelessWidget {
+//   const HomePageGrid({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: GridView.count(
+//         crossAxisCount: 2,
+//         children: [
+//           Padding(
+//             padding: EdgeInsets.all(8.0),
+//             child: Container(
+//               height: 50,
+//               width: 50,
+//               color: Colors.blue,
+//             ),
+//           ),
+//           Padding(
+//             padding: EdgeInsets.all(8.0),
+//             child: Container(
+//               height: 50,
+//               width: 50,
+//               color: Colors.blue,
+//             ),
+//           ),
+//           Padding(
+//             padding: EdgeInsets.all(8.0),
+//             child: Container(
+//               height: 50,
+//               width: 50,
+//               color: Colors.blue,
+//             ),
+//           ),
+//           Padding(
+//             padding: EdgeInsets.all(8.0),
+//             child: Container(
+//               height: 50,
+//               width: 50,
+//               color: Colors.blue,
+//             ),
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
